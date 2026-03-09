b87f95a9c896fedca8a2961cb1bee050cd18613e
Strakhov Nicholas <strahovnikolaj27@gmail.com>
2026-03-09 22:23:49 +0300

    add remaining files

--- main-service/src/main/java/ru/practicum/MainServiceApplication.java
+++ main-service/src/main/java/ru/practicum/MainServiceApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EwmStatsServer {
+public class MainServiceApplication {
     public static void main(String[] args) {
-        SpringApplication.run(EwmStatsServer.class, args);
+        SpringApplication.run(MainServiceApplication.class, args);
     }
 }
