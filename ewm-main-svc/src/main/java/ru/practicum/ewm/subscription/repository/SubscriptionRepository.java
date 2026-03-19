cf81b4c862a607933b7adc2b725bba34a708d5df
Nicholas <strahovnikolaj27@gmail.com>
2026-03-19 18:10:45 +0300

    feat: checkstyke

--- ewm-main-svc/src/main/java/ru/practicum/ewm/subscription/repository/SubscriptionRepository.java
+++ ewm-main-svc/src/main/java/ru/practicum/ewm/subscription/repository/SubscriptionRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import ru.practicum.ewm.enums.FriendshipsStatus;
 import ru.practicum.ewm.subscription.model.Subscription;
 import ru.practicum.ewm.user.model.User;
